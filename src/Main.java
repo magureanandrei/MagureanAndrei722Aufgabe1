5d2b0fe8a5b270dd16d3ff0a114982d131f44e1b
magureanandrei <andimagurean714@gmail.com>
2025-01-15 16:23:22 +0200

    Last commit(b si c)

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,7 +16,7 @@ public class Main {
         try (BufferedReader reader = Files.newBufferedReader(Paths.get(path))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] teile = line.split("&");
+                String[] teile = line.split("   ");
                 list.add(teile);
             }
         } catch (IOException e) {
@@ -34,12 +31,23 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Geben Sie einen Großbuchstaben ein: ");
         String letter = scanner.nextLine().toUpperCase();
-        for (String[] eintrag :list) {
-            String name = eintrag[1];
+        for (String[] l :list) {
+            System.out.println(Arrays.toString(l));
+            String name = l[1];
             if (name.toUpperCase().startsWith(letter)) {
                 System.out.println(name);
             }
         }
+
+        //c
+        List<String> Sorted= new ArrayList<>();
+        for(String[] eintrag : list) {
+            if(eintrag[2].equals("Fieber")) {
+                Sorted.add(eintrag[1]);
+            }
+        }
+        Collections.sort(Sorted);
+        System.out.println(Sorted);
 
     }
 }
