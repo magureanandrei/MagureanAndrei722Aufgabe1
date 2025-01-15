d6ec10086b29eb76a1e46507a26272e8d6b54a1a
magureanandrei <andimagurean714@gmail.com>
2025-01-15 16:14:45 +0200

    subpunct a si b

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //a
+        String path = System.getProperty("user.dir") + "\\src\\file.tsv";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        List<String[]> list = new ArrayList<>();
+
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(path))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] teile = line.split("&");
+                list.add(teile);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        for (String[] eintrag : list) {
+            System.out.println(Arrays.toString(eintrag));
+        }
+
+        //b
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Geben Sie einen Großbuchstaben ein: ");
+        String letter = scanner.nextLine().toUpperCase();
+        for (String[] eintrag :list) {
+            String name = eintrag[1];
+            if (name.toUpperCase().startsWith(letter)) {
+                System.out.println(name);
+            }
+        }
+
     }
 }
